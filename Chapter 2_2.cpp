04a9996b9dfca7807dce78d77cddb850b637ef71
Zhao Wei <zhaowei.pku@gmail.com>
2015-11-07 00:16:29 +0800

    Pracitce 2.4
    受到正文启发，想到了自己的百度二面题目。可以按照这个squeeze函数来做，又简单又高效。只是在传入为char*字符串时会报错。应该再研究一下。

---
+++ Chapter 2_2.cpp
@@ -0,0 +1,24 @@
+﻿///@file	Practice 2.4
+///@author	zhaowei
+///@date	2015.11.06
+#include <stdio.h>
+
+///@brief	清除字符串之间的多余空格符
+///@note	时间复杂度为O(n)，空间复杂度为O(1)
+void squeeze (char s[]) {
+	int i = 0, j = 0;
+	while(s[i] != '\0') {
+		if (s[i] != ' ' || (s[i] == ' ' && i > 0 && s[i-1] != ' ')) 
+			s[j++] = s[i];					
+		i++;
+	}
+	s[j] = '\0';
+}
+
+int main() {
+	char s[] = "hello   world   bye-bye    yike";	
+	printf("%s\n", s);
+	squeeze(s);
+	printf("%s\n", s);
+	return 0;
+}
