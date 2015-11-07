4edffc4b2140658c6e7f1644d842e590dc4afbc5
Zhao Wei <zhaowei.pku@gmail.com>
2015-11-07 22:16:58 +0800

    Practice 2.7
    1. 类似Practice 2.6，通过异或完成。

---
+++ Practice 2_7.cpp
@@ -0,0 +1,15 @@
+﻿///@file	Practice 2.7
+///@author	zhaowei
+///@date	2015.11.07
+
+///@brief	将x从p开始的n位求反，其余各位保持不变
+///@note	类似Practice2.6，构造一个数，使其从p开始的n位均为1，其余各位为0，然后让x与其异或即可。
+int invert(const int& x, const int& p, const int& n) {
+	if (p < n)	return -1;
+	return (~(~0 << n) << (p - n + 1)) ^ x;
+}
+
+int main() {
+	int rslt = invert(19, 2, 2);
+	return 0;
+}
