828ca9c643fd65e6470c3e4384c652a1f6e1948e
Zhao Wei <zhaowei.pku@gmail.com>
2015-11-07 22:55:11 +0800

    Practice 2.10
    1. 用条件表达式重写lower()函数

---
+++ Practice 2_10.cpp
@@ -0,0 +1,17 @@
+﻿///@file	Pracitce 2.10
+///@author	zhaowei
+///@date	2015.11.07
+#include <stdio.h>
+
+///@brief	用条件表达式重写lower()函数
+char lower(char s) {
+	return s <= 'Z' && s >= 'A' ? s - 'A' + 'a' : s;		
+}
+
+int main() {
+	int c = 0;
+	while ((c = getchar()) != EOF) {
+		printf("%c", lower(c));
+	}
+	return 0;
+}
