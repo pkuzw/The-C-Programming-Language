a73766fe3daf30707dfed8cafbd2ffedcfe48cc0
Zhao Wei <zhaowei.pku@gmail.com>
2015-11-07 22:16:31 +0800

    Practice 2.6
    1. 防御性编程。

--- Practice 2_6.cpp
+++ Practice 2_6.cpp
@@ -5,7 +5,8 @@
 
 ///@brief	将x中从第p位（从右起）开始的n位设置为y中最右边的n位。假设最右边的一位是第0位.
 ///@note	先将x中的这n位置为0，然后把y的最右n位移动到对应位置，两者做或运算。
-int setbits(int x, int p, int n, int y) {
+int setbits(const int& x, const int& p, const int& n, const int& y) {
+	if (p < n)	return -1;
 	return (x & ~(~(~0 << n) << (p - n + 1))) | ((y & ~(~0 << n)) << (p - n + 1));
 }
 
