ada0619b49ae56ffe297034dcf5fdee03a2712cc
Zhao Wei <zhaowei.pku@gmail.com>
2015-11-07 22:45:53 +0800

    Practice 2.10
    1. x &= x - 1可以删除x中最靠右的1.

---
+++ Practice 2_9.cpp
@@ -0,0 +1,18 @@
+﻿///@file	Practice 2.9
+///@author	zhaowei
+///@date	2015.11.07
+
+///@brief	利用x &= x - 1来计算x中1位的个数。
+int bitcount(unsigned x) {
+	int cnt = 0;
+	while (x) {
+		x &= x - 1;
+		cnt++;
+	}
+	return cnt;
+}
+
+int main() {
+	int rslt = bitcount(31);
+	return 0;
+}
